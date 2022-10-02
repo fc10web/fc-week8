b344f7e80863c6f98a7332c282d221f91f506b0d
fc10web <fcvcguutt@gmail.com>
2022-10-02 09:10:45 +0000

    week8

--- LogRocket/20220615-random/hardhat.config.js
+++ LogRocket/20220615-random/hardhat.config.js
@@ -19,25 +19,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 /**
- * @type import('hardhat/config').HardhatUserConfig
+ * 
  */
 
 module.exports = {
   solidity: "0.8.4",
-/*
   networks: {
-    "local-devnode": {
-       url: "http://localhost:8545",
-       accounts: { mnemonic: "test test test test test test test test test test test junk" }
-    },
-    "optimistic-kovan": {
-       url: "https://kovan.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
-    },
+    
     "optimism": {
-       url: "https://mainnet.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
-    }
+       url: "https://opt-goerli.g.alchemy.com/v2/PpmvfWiBaBqc55WoZVuPhy6Z6NBYhsbG",
+       accounts: [ "
   }
-*/
 };
